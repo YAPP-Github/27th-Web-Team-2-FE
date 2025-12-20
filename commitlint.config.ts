ea07de9f8ce2de5eb2445bc306933b4d35581688
jaeml06 <jaeml0630@gmail.com>
2025-12-20 19:36:55 +0900

    config: 커밋 규칙/훅/타입체크 적용되도록 설정

---
+++ commitlint.config.ts
@@ -0,0 +1,24 @@
+import type { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'refactor',
+        'style',
+        'test',
+        'design',
+        'config',
+      ],
+    ],
+  },
+};
+
+export default config;
