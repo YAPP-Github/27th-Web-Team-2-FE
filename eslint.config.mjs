89d711258aee200e541e9870c5713400514ea1ca
jaeml06 <jaeml0630@gmail.com>
2025-12-20 17:28:56 +0900

    chore: Prettier와 ESLint에 Tailwind 클래스 정렬 호환성 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import storybook from "eslint-plugin-storybook";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...storybook.configs["flat/recommended"],
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -16,6 +17,8 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  // Let Prettier handle formatting rules.
+  prettier,
 ]);
 
 export default eslintConfig;
