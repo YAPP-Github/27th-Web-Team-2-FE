8c83b060c6b1e1d7f1d93a548b7bcdd572c741aa
JAEMIN LEE <jaeml0630@gmail.com>
2026-01-08 20:59:14 +0900

    [CHORE] Tailwind 디자인 시스템 적용 (#13)
    * config: tailwind 디자인시스템 적용
    
    * test: tailwind 적용 테스트 컴포넌트 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import TestComponent from '@/components/TestComponent';
+
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return <TestComponent />;
 }
