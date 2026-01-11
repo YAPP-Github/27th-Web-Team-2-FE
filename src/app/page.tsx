c60a48d95cf94b08bf8f67d7fc399c193d194d8f
yejinleee <yejin9487@gmail.com>
2026-01-11 13:51:32 +0900

    feat: 라우팅 구조 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import TestComponent from '@/components/TestComponent';
-
-export default function Home() {
-  return <TestComponent />;
+export default function OnboardingPage() {
+  return (
+    <div className='flex min-h-screen items-center justify-center'>
+      <div className='text-center'>
+        <h1 className='text-headline-1 text-text-primary mb-4'>
+          온보딩 페이지
+        </h1>
+      </div>
+    </div>
+  );
 }
