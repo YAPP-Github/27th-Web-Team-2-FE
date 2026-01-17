32ed0871be137259d602d3b2b2f988a1fa4439ad
yejinleee <yejin9487@gmail.com>
2026-01-17 16:46:47 +0900

    fix: 환경별 proxy

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,7 +1,10 @@
 import ky from 'ky';
 
 export const api = ky.create({
-  prefixUrl: '/proxy-api',
+  prefixUrl:
+    typeof window === 'undefined'
+      ? process.env.NEXT_PUBLIC_API_BASE_URL_SANDBOX
+      : '/proxy-api',
   headers: {
     'Content-Type': 'application/json',
   },
