812e003592c3299ead8fd10773639f8db5c41e4e
jaeml06 <jaeml0630@gmail.com>
2026-01-28 00:17:56 +0900

    style: toast 스타일링 변경

--- src/shared/ui/toast/Toast.tsx
+++ src/shared/ui/toast/Toast.tsx
@@ -21,11 +21,11 @@ export default function Toast({
   // 기본 클래스: Pill shape, White background, Shadow
   // Tailwind Utility Class 사용
   const baseClasses =
-    'inline-flex w-fit items-center gap-2 rounded-full bg-gray-0 px-5 py-3 shadow-[0px_0px_12px_0px_rgba(0,0,0,0.1)]';
+    'inline-flex w-fit items-center gap-1.5 rounded-full bg-gray-0 pl-[12px] pr-[16px] py-[10px] shadow-[0px_4px_34px_0px_rgba(0,0,0,0.1)]';
 
   // 텍스트 스타일: text-primary
-  // Pretendard 폰트 특성상 영문/숫자 대비 한글이 시각적으로 약간 위로 떠보일 수 있어 pt-[2px]로 보정
-  const textClass = 'text-text-primary text-title-8 pt-[2px]';
+  // Pretendard 폰트 특성상 영문/숫자 대비 한글이 시각적으로 약간 위로 떠보일 수 있어 pt-[2px]로 보정 -> Figma 중앙 정렬 기준 제거 test
+  const textClass = 'text-text-primary text-title-8';
 
   const combinedClasses = `${baseClasses} ${className}`;
 
@@ -42,7 +42,7 @@ export default function Toast({
         ) : (
           <Icon
             name='ic_circle_check_filled'
-            size={24}
+            size={20}
             className='text-primary-default'
           />
         )}
